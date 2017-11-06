d92795cf8363bb51f3ebef5f67ce405e174955ee
beautifulxiaojian <xiaojiannangkang@126.com>
2017-11-06 16:12:20 +0800

    update head

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  loginStatus = false;
+  loginStatus = true;
 
   menuCanLoad(): boolean {
     return this.loginStatus;
