7178f5ef23d740d7a8b233b26458646c39c9d469
shaobo <929029960@qq.com>
2017-11-28 09:48:01 +0800

    test excel reader

--- src/app/modules/upload/pages/video/video.component.ts
+++ src/app/modules/upload/pages/video/video.component.ts
@@ -60,7 +60,41 @@ export class VideoComponent {
       const result = new Array<any>();
       workbook.SheetNames.forEach(sheetName => {
         const roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
-        if (roa.length) { result.push(roa); }
+        if (roa.length) {
+          result.push(roa);
+          const test = roa;
+          const temp = {};
+          test.forEach(element => {
+            if (element['知识层级（一级）']) {
+              temp[element['知识层级（一级）']] = {};
+            }
+          });
+
+          let level_1 = '';
+
+          test.forEach(element => {
+            if (element['知识层级（一级）']) {
+              level_1 = element['知识层级（一级）'];
+            }
+            if (element['知识层级（二级）']) {
+              temp[level_1][element['知识层级（二级）']] = new Array<string>();
+            }
+          });
+
+          let level_2 = '';
+
+          test.forEach(element => {
+            if (element['知识层级（一级）']) {
+              level_1 = element['知识层级（一级）'];
+            }
+            if (element['知识层级（二级）']) {
+              level_2 = element['知识层级（二级）'];
+            }
+            temp[level_1][level_2].push(element['知识层级（三级）']);
+          });
+
+          console.log(JSON.stringify(temp));
+        }
       });
       try {
         for (const key in result[0][0]) {
@@ -75,4 +109,24 @@ export class VideoComponent {
     });
     reader.readAsArrayBuffer(file);
   }
+  // readExcel(file: File) {
+  //   this.theads = [];
+  //   const reader = new FileReader();
+  //   reader.addEventListener('load', event => {
+  //     const data = new Uint8Array(reader.result);
+  //     const workbook = XLSX.read(data, { type: 'array' });
+  //     const result = new Array<any>();
+  //     if (workbook.SheetNames.length !== 2) {
+  //       console.error('error excel file');
+  //     }
+  //     workbook.SheetNames.forEach(sheetName => {
+  //       console.log('reade' + sheetName);
+  //       const worksheet = workbook.Sheets[sheetName];
+  //       const desired_cell = worksheet['A1'];
+  //       console.log(worksheet);
+  //       console.log(desired_cell);
+  //     });
+  //   });
+  //   reader.readAsArrayBuffer(file);
+  // }
 }
